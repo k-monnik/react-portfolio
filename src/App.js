bdb6954aaffa0fec8a8d4dff3dc8728b93929748
Kristen Monnik <nolaviolinist@gmail.com>
2021-12-22 14:55:43 -0500

    add About Projects Contact components to router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Kristen's future portfolio</h1>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <About />
+      </Route>
+      <Route path="/" exact>
+        <Projects />
+      </Route>
+      <Route path="/" exact>
+        <Contact />
+      </Route>
+    </Router>
+
+    // { /* <div className="container">
+    //   <h1>Kristen's future portfolio</h1>
+    // </div> */}
   );
 }
 
