4d07ec9ee4ea45e952ee9fed40a3a0b010f8e803
Kristen Monnik <nolaviolinist@gmail.com>
2022-03-25 23:35:00 -0400

    add initial styling to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,39 +36,65 @@ function Contact() {
 
 
     return (
-        <div id="contact">
-            <p>contact me here</p>
-            <div>
+        <div id="contact" className="h-1/2">
+            <p className="m-14 font-regular text-5xl flex justify-center">Let's chat!</p>
+            <div className="p-6 h-96 w-3/5 border border-blue-700 rounded flex justify-center mx-auto">
                 <form onSubmit={onSubmit}>
-                    <input
-                        type='text'
-                        name='from_name'
-                        placeholder='from name'
-                        value={toSend.from_name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='to_name'
-                        placeholder='to name'
-                        value={toSend.to_name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='message'
-                        placeholder='Your message'
-                        value={toSend.message}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='reply_to'
-                        placeholder='Your email'
-                        value={toSend.reply_to}
-                        onChange={handleChange}
-                    />
-                    <button className="bg-blue-400 p-1 rounded-md cursor-pointer" type='submit'>Submit</button>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="flex flex-col border border-blue-700 rounded">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                type='text'
+                                name='from_name'
+                                placeholder='Name'
+                                value={toSend.from_name}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex flex-col border border-blue-700 rounded">
+                            <label htmlFor="return email">Email</label>
+                            <input
+                                type='text'
+                                name='reply_to'
+                                placeholder='Email'
+                                value={toSend.reply_to}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex flex-col col-span-2 border border-blue-700 rounded">
+                            <label htmlFor="subject">Subject</label>
+                            <input
+                                type='text'
+                                name='subject'
+                                placeholder='Subject'
+                                value={toSend.to_name}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex flex-col col-span-2 border border-blue-700 rounded">
+                            <label htmlFor="message">
+                                <div className="flex align-items">
+                                    Message
+                                    {/* <span className="ml-auto opacity-75">Max. 500 characters</span> */}
+                                </div>
+                            </label>
+                            {/* <textarea maxLength="500" rows="4" type="text" id="subject" name="subject" className="form-input px-3 py-2 rounded-md" required /> */}
+                            <input
+                                type='text'
+                                name='message'
+                                placeholder='message'
+                                value={toSend.message}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex justify-end py-4">
+                            <button className="bg-blue-400 p-1 rounded-md cursor-pointer text-xl" type='submit'>
+                                Submit
+                            </button>
+                        </div>
+
+                    </div>
+
                 </form>
 
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import Cards from "./Cards"
 
 const Projects = () => {
     return (
-        <div id="projects">
+        <div id="projects" className="h-screen">
             <h1 className='mt-12 mb-2 font-regular text-5xl flex justify-center content-center'>Projects</h1>
             <p className="mt-2 mb-36 font-thin text-2xl flex justify-center">Check out what I've been working on lately:</p>
             <Cards />
