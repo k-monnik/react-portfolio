3de99a218870966852eaab1d406fa1b7a9d3e948
Kristen Monnik <nolaviolinist@gmail.com>
2022-03-25 23:00:46 -0400

    add initial styling for projects and cards components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,27 +1,27 @@
 import { FaGithub, FaImage } from "react-icons/fa";
-import shoplist from "../assets/sshoplist-img.jpg";
-import spacepic from "../assets/spacestagram-img.png";
+import shoplist from "../assets/shoplist-img.png";
+import spacepic from "../assets/space-img.png";
 import tasklist from "../assets/task-tracker-img.png";
 
 const Cards = () => {
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="m-12 grid grid-cols-1 md:grid-cols-3">
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src={shoplist} alt="" className="w-full" />
+                <img src={shoplist} alt="" className="" />
                 <div className="px-6 py-4">
-                    <div className="font-bold text-purple-500 text-xl">
+                    <div className="font-bold text-blue-800 text-xl flex justify-center">
                         Smart Shopping List
                     </div>
                     <div>
-                        <p>
+                        <p className="flex justify-center text-lg font-light mb-6">
                             React/Firebase/Tailwind
                         </p>
-                        <p>
-                            A web app that lets the user create and share shopping lists that track item purchase dates, and estimates how soon they will need to be purchased again.
+                        <p className="text-normal font-thin mb-8">
+                            A web app designed with a chalkboard theme that lets the user create and share shopping lists that track item purchase dates, and estimates how soon they will need to be purchased again.
                         </p>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className="flex flex-row justify-center gap-12">
                         <button
                             className="flex p-2 rounded-lg bg-blue-500"
                             onClick={() => { window.open("https://tcl-38-smart-shopping-list.web.app/") }}
@@ -35,20 +35,20 @@ const Cards = () => {
 
             </div>
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src={spacepic} alt="" className="w-full" />
+                <img src={spacepic} alt="" className="" />
                 <div className="px-6 py-4">
-                    <div className="font-bold text-purple-500 text-xl">
+                    <div className="font-bold text-blue-800 text-xl flex justify-center">
                         Spacestagram
                     </div>
                     <div>
-                        <p>
+                        <p className="flex justify-center text-lg font-light mb-6">
                             React/CSS
                         </p>
-                        <p>
+                        <p className="text-normal font-thin mb-14">
                             A web app that fetches and displays images from NASA's Astronomy Photo of the Day API, and gives users the option to "like" or "unlike" the image.
                         </p>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className="flex flex-row justify-center gap-12">
                         <button
                             className="flex p-2 rounded-lg bg-blue-500"
                             onClick={() => { window.open("https://km-spacestagram.herokuapp.com/") }}
@@ -64,18 +64,18 @@ const Cards = () => {
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img src={tasklist} alt="" className="w-full" />
                 <div className="px-6 py-4">
-                    <div className="font-bold text-purple-500 text-xl">
+                    <div className="font-bold text-blue-800 text-xl flex justify-center">
                         Task Tracker
                     </div>
                     <div>
-                        <p>
+                        <p className="flex justify-center text-lg font-light mb-6">
                             React/CSS
                         </p>
-                        <p>
+                        <p className="text-normal font-thin mb-20">
                             A web app that lets the user add, delete, and create reminders for tasks on their to-do list.
                         </p>
                     </div>
-                    <div className="flex flex-row justify-between">
+                    <div className="flex flex-row justify-center gap-12">
                         <button
                             className="flex p-2 rounded-lg bg-blue-500"
                             onClick={() => { window.open("https://monnik-react-task-tracker.netlify.app/") }}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,8 @@ import Cards from "./Cards"
 const Projects = () => {
     return (
         <div id="projects">
-            <h1>Projects</h1>
+            <h1 className='mt-12 mb-2 font-regular text-5xl flex justify-center content-center'>Projects</h1>
+            <p className="mt-2 mb-36 font-thin text-2xl flex justify-center">Check out what I've been working on lately:</p>
             <Cards />
 
         </div>
