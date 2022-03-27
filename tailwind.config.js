a36e7aea4b1fbad224f628b512f33bd87901c198
Kristen Monnik <nolaviolinist@gmail.com>
2022-03-27 00:04:00 -0400

    add custom color palette to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'ocean': '#006466',
+      'midnight': '#1B3A4B',
+      'plum': '#4D194D',
+      'silver': '#9999A1',
+      'ice': '#E6E6E9',
+    },
     extend: {
       fontFamily: {
         Josefin: ["Josefin Sans", "sans-serif"],
