a0645de2837c607fd9bb9d154d16ea40da7303a2
Kristen Monnik <nolaviolinist@gmail.com>
2022-04-05 17:38:53 -0400

    add mobile menu icon and initial styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 // import { Link, NavLink } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import { FiMenu } from "react-icons/fi";
 
 const Nav = () => {
     return (
@@ -15,7 +16,11 @@ const Nav = () => {
                 <div className='font-bold m-2 font-Josefin'>
                     <HashLink smooth to='/#'>monnik.dev</HashLink>
                 </div>
-                <div>
+                <div className='px-6 mt-2 text-2xl md:hidden'>
+                    <FiMenu />
+                </div>
+
+                <div className='hidden md:block'>
                     <ul className='flex flex-row gap-20 mr-8'>
                         <li className='pt-2 hover:border-b-4 hover:border-midnight'>
                             <HashLink smooth to={'/page#about'}>About</HashLink>
